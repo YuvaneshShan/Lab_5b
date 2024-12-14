ef2df6951342e986c7891a50c3d989a0205ecd62
YuvaneshShan <ci230123@student.uthm.edu.my>
2024-12-14 22:12:36 +0800

    Add files via upload

---
+++ save_update.php
@@ -0,0 +1,19 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "Lab_5b");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $matric = $_POST["matric"];
+    $name = $_POST["name"];
+    $role = $_POST["role"];
+
+    $sql = "UPDATE users SET name='$name', role='$role' WHERE matric='$matric'";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: display_users.php");
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
